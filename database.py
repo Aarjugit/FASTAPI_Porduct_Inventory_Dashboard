c6f403e23b8a34c3d1c9fe30bb8b2de1b73754f6
Aarjugit <aarjugoswami19@gmail.com>
2026-01-13 19:59:42 +0530

    change pw in file

--- database.py
+++ database.py
@@ -1,6 +1,7 @@
 from sqlalchemy import create_engine 
 from sqlalchemy.orm import sessionmaker 
 
-db_url = "postgresql://postgres:123456@localhost:5432/FastAPI"
+db_url = "postgresql://postgres:your_password@localhost:5432/FastAPI"
 engine=create_engine(db_url)
 session =  sessionmaker(autocommit=False,autoflush=False, bind=engine)
+
